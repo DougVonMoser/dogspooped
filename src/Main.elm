c44d59080fb8f20db7eef220e380f646c3cff3b3
Doug VonMoser <dvonmoser@cars.com>
2018-09-13 09:23:17 -0500

    take your fun fun

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,12 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (text)
+import Time exposing (Posix)
+import Css exposing (..)
+import Html
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (css, href, src, class)
+import Html.Styled.Events exposing (onClick)
 
 
 type MealStatus
@@ -11,8 +16,8 @@ type MealStatus
 
 
 type alias Dog =
-    { poops : List Int
-    , pees : List Int
+    { poops : List Posix
+    , pees : List Posix
     , allergied : Bool
     , breakfast : MealStatus
     , dinner : MealStatus
@@ -26,20 +31,63 @@ type alias Model =
 
 initModel : Model
 initModel =
-    []
+    [ { poops = [ Time.millisToPosix 1536847616243 ]
+      , pees = []
+      , allergied = False
+      , breakfast = HaveNot
+      , dinner = HaveNot
+      , name = "Oakley"
+      }
+    , { poops = []
+      , pees = []
+      , allergied = False
+      , breakfast = HaveNot
+      , dinner = HaveNot
+      , name = "Easton"
+      }
+    ]
+
+
+type Msg
+    = Msg
 
 
 update msg model =
     []
 
 
+viewHour : Posix -> String
+viewHour posix =
+    Time.toHour Time.utc posix
+        |> String.fromInt
+
+
+viewWaste : String -> List Posix -> Html Msg
+viewWaste wasteAction wastes =
+    div [ class wasteAction, css [ paddingLeft (px 16) ] ]
+        [ span [] [ text wasteAction ]
+        , span [] (List.map (viewHour >> text) wastes)
+        ]
+
+
+dogView : Dog -> Html Msg
+dogView dog =
+    div [ css [ border3 (px 1) dashed (rgb 12 12 12), margin (px 16) ] ]
+        [ text dog.name
+        , viewWaste "poops" dog.poops
+        , viewWaste "pees" dog.pees
+        ]
+
+
+view : Model -> Html Msg
 view model =
-    text "working!"
+    List.map dogView model
+        |> div []
 
 
 main =
     Browser.sandbox
         { init = initModel
-        , view = view
+        , view = view >> toUnstyled
         , update = update
         }
